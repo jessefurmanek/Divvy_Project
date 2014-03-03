5fc00109ac9ea280c86227e7c5c25c4d35467c33
jessefurmanek <jesse.furmanek@gmail.com>
2014-03-02 20:59:45 -0600

    parsing .csv using python-- thought you'd like that @alexainslie !

---
+++ csvParse.py
@@ -0,0 +1,11 @@
+import csv
+import json
+
+csvfile = open('Divvy_Stations_2013.csv', 'r')
+jsonfile = open('divvy_stations.json', 'w')
+
+fieldnames = ("FirstName","LastName","IDNumber","Message")
+reader = csv.reader( csvfile)
+for row in reader:
+    json.dump(row, jsonfile)
+    jsonfile.write('\n'.join(','))
